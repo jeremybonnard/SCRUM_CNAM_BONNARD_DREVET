1a07b8ffacb425f7d11f892695b5f44d12dba9fa
VincentDrevet <vi.drevet@gmail.com>
2016-02-05 12:19:47 +0100

    test

---
+++ src/FR/CNAM/NFP121/BONNARDDREVET/SCRUM/Main.java
@@ -0,0 +1,10 @@
+package FR.CNAM.NFP121.BONNARDDREVET.SCRUM;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/FR/CNAM/NFP121/BONNARDDREVET/SCRUM/gui/Fenetre.java
@@ -0,0 +1,13 @@
+package FR.CNAM.NFP121.BONNARDDREVET.SCRUM.gui;
+
+import javax.swing.JApplet;
+
+public class Fenetre extends JApplet {
+
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
