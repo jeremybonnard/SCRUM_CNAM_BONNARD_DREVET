b1b091485c906572b14ba11095825e72f793594d
Jérémy BONNARD <jeremybonnard42@gmail.com>
2016-03-09 22:40:18 +0100

    Ajout des fonction allUtilisateur et enregistrerUtilisateur + Serializable sur tt les classe

--- src/FR/CNAM/NFP121/BONNARDDREVET/SCRUM/Main.java
+++ src/FR/CNAM/NFP121/BONNARDDREVET/SCRUM/Main.java
@@ -1,11 +1,15 @@
 package FR.CNAM.NFP121.BONNARDDREVET.SCRUM;
 
+import java.io.IOException;
+
+import FR.CNAM.NFP121.BONNARDDREVET.SCRUM.business.Utilisateur;
+import FR.CNAM.NFP121.BONNARDDREVET.SCRUM.dao.UtilisateurUtil;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		
+
 		
 		
 
