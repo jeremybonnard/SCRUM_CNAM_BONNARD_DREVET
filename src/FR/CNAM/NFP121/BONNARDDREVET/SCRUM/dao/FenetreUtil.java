d954e0bf22954547f351656d33641824f82bf92c
Jérémy BONNARD <jeremybonnard42@gmail.com>
2016-03-09 23:59:43 +0100

    Modif sur AjoutUtilisateur

---
+++ src/FR/CNAM/NFP121/BONNARDDREVET/SCRUM/dao/FenetreUtil.java
@@ -0,0 +1,15 @@
+package FR.CNAM.NFP121.BONNARDDREVET.SCRUM.dao;
+
+import java.awt.MenuItem;
+
+import javax.swing.JMenuItem;
+
+import FR.CNAM.NFP121.BONNARDDREVET.SCRUM.gui.Fenetre;
+
+public class FenetreUtil {
+
+	public static void actualiserUtilisateur()
+	{
+		// A faire
+	}
+}
