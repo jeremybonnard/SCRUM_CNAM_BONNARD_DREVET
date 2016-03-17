b31c33f8d8ec566e265e45fbe296f6850cbd9d69
VincentDrevet <vi.drevet@gmail.com>
2016-03-17 12:29:54 +0100

    ajout de la javadoc pour StatusUtil

--- src/FR/CNAM/NFP121/BONNARDDREVET/SCRUM/dao/StatusUtil.java
+++ src/FR/CNAM/NFP121/BONNARDDREVET/SCRUM/dao/StatusUtil.java
@@ -2,7 +2,17 @@ package FR.CNAM.NFP121.BONNARDDREVET.SCRUM.dao;
 
 import FR.CNAM.NFP121.BONNARDDREVET.SCRUM.business.Status;
 
+/**
+ * StatusUtil est la classe  permettant d'interagir avec les objets de Status
+ */
+
 public class StatusUtil {
+	
+	/**
+	 * Cette méthode definit l'id et l'action a effectuer initialement
+	 * @return Statuc(0,"A Faire")
+	 * 	retourn l'id et l'action de la tache créer
+	
 	
 	public static Status getStatusDepart()
 	{
