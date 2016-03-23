554d091fedf22bdbf3900974cd306e8e518224bd
VincentDrevet <vi.drevet@gmail.com>
2016-03-23 13:40:41 +0100

    Ajout javadoc pour email validator

--- src/FR/CNAM/NFP121/BONNARDDREVET/SCRUM/dao/EmailValidator.java
+++ src/FR/CNAM/NFP121/BONNARDDREVET/SCRUM/dao/EmailValidator.java
@@ -1,6 +1,23 @@
 package FR.CNAM.NFP121.BONNARDDREVET.SCRUM.dao;
 
+
+/**
+ * La classe EmailValidator permet la verification des adresse mail.
+ */
+
+
+  
 public class EmailValidator {
+	
+	
+	
+	/**
+	 * La methode est Valide permet la verification d'une adresse mail au moyen d'une expression regulière (regex)
+	 * @return true ou false
+	 * 	Retourne vrai ou faux si la condition est respecté ou non.
+	 * @param email
+	 * 	Prend en parametre une adresse email.
+	 */
 	public boolean estValide(String email){
 		if(email.matches("^(.*)@(.*)\\.(.{2,4})$")){
 			return true;
